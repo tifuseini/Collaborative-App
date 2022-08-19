2bd3c1fba2cf809c84de77c78237a2d3b21f0838
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-19 22:55:57 +0000

    Initial commit

---
+++ src/main/java/com/example/reactiveproject/ReactiveprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactiveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactiveproject/ReactiveprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
